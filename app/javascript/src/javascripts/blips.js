970255f5bb90a2d34263a82a35654342c5efcd92
catt0s <meow@catt0s.win>
2023-11-29 20:59:06 -0600

    idk

--- app/javascript/src/javascripts/blips.js
+++ app/javascript/src/javascripts/blips.js
@@ -43,6 +43,29 @@ ${stripped_body}
   });
 };
 
+Blip.block = function(e) {
+  e.preventDefault();
+  if (!confirm("Are you sure you want to blacklist this user? Their posts will be blacklisted as well. Additionally, all blacklisted users will also have both their posts and non-posts blocked.")) return;
+  const parent = $(e.target).parents('article.comment');
+  const buid = parent.data('creator-id');
+  $.ajax({
+    url:`/users/${Utility.meta("current-user-id")}.json`, 
+    type: 'PATCH',
+    data:{
+      'user[blacklist_users]':true,
+      'user[blacklisted_tags]':`\nuser:${buid}`
+    },
+    headers: {
+      accept: '*/*;q=0.5,text/javascript',
+      Cookie: document.cookie,
+    }
+  }).done(function(data) {
+    $(window).trigger("danbooru:notice", "User blocked");
+  }).fail(function (data) {
+    Utility.error("Failed to block user.");
+  });
+};
+
 Blip.initialize_all = function() {
   if($("#c-blips").length) {
     $(".blip-atme-link").on('click', e => {
